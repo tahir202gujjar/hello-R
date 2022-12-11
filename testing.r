0dde451b02988a68cce4ba4ae4aa9354e86391ac
Tahir Abbas <tahirabbas337@gmail.com>
2022-12-11 18:08:20 +0500

    few lines add

--- testing.r
+++ testing.r
@@ -1,4 +1,6 @@
 this is Github
+print("class five")
+
 print("hello frm Github")
 
 name = "tahir"
@@ -6,3 +8,7 @@ version = "Rstudio - in writing"
 
 print(name)
 print("these lines have been written in studio")
+
+name_t = "tahir"
+print(name_t)
+
